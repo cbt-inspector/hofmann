9a231dbf7f23428ae1580349bd89ef7f1043005d
sussy-balls-impostor <eo2dog192@gmail.com>
2022-02-08 20:52:54 +0100

    bepis2 (epic revamp)

--- hofmann/main.py
+++ hofmann/main.py
@@ -1,10 +1,48 @@
 from flask import Flask , render_template
+import os
 
 app = Flask(__name__)
 
+
+
+
 @app.route("/")
 def home() :
-    return render_template("index.html")
+    #path = f"{os.getcwd()}/static/bruh/".replace("\\", "/")
+    #tempcontents = os.listdir(path)
+    #dircontents = []
+    #for x in tempcontents:
+    #    if "Klasse" in x:
+    #        sus = x.split("Klasse")
+    #        dircontents.append(f"{sus[1]}. Jahrgangsstufe")
+    #    elif x == "EA":
+    #        dircontents.append("Engine Alpha")
+    #    else:
+    #        dircontents.append(x)
+    #dircontents.sort()
+    dircontents = ["6. Jahrgangsstufe","7. Jahrgangsstufe","9. Jahrgangsstufe","10. Jahrgangsstufe","11. Jahrgangsstufe","12. Jahrgangsstufe","Engine Alpha",]
+    return render_template("index.html", dircontents= dircontents)
+
+
+@app.route("/<bruh>")
+def folder(bruh) :
+    #contents= []
+    #path = f"{os.getcwd()}/static/bruh/"
+    #for x in [os.path.join(root, name).split(path)
+    #         for root, dirs, files in os.walk(path)
+    #         for name in files]:
+    #             e= x[1].replace("\\","/")
+    #             contents.append(e)
+    #
+    #dir_list = next(os.walk(path))[1]
+    #
+    #
+    #return ''.join([f"{str(x)}-- " for x in dir_list])
+    return bruh
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
