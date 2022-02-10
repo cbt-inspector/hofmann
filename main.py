60653ba64746d1d25e195ced7f830c5d224a88a7
sussy-balls-impostor <eo2dog192@gmail.com>
2022-02-10 21:19:48 +0100

    404 (but it doesnt work)

--- main.py
+++ main.py
@@ -130,11 +130,12 @@ def update():
     repo.remotes.origin.pull()
     return home()
 
-
+@app.route("/error")
+def error():
+    return render_template("404.html")
 #@app.errorhandler("404")
-#def page_not_found(e):
-    # note that we set the 404 status explicitly
- #   return render_template('404.html'), 404
+#def page_not_found(e):   # note that we set the 404 status explicitly
+#   return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
