ebb6b627f096eb4a7c0adb06d3467f799e339e18
moehreag <moehreag@gmail.com>
2022-02-10 15:38:30 +0100

    cleanup & documentation

--- main.py
+++ main.py
@@ -1,25 +1,85 @@
+#!/usr/bin/env python3
 from flask import Flask , render_template, send_from_directory
 import os
 
 app = Flask(__name__)
 
 
+'''
+Main script for the website of Joachim Hofmann
 
+Functions in files:
+- main.py:
+    - routing of requests via flask
+    - returning HTML files with jinja2 arguments
+    - serving these files to the user
+
+- requirements.txt
+    - text file for all required Python modules for easy installation
+
+- app.ini
+    - Init file for an uwsgi server (with arguments)
+
+- styles.css
+    - CSS file for shared styles
+
+- templates folder:
+
+    - base.html
+    - base template for the webpage
+    - including JS functions for e.g. the darkmode
+    - CSS stylings
+
+    - index.html
+        - the main page including building up the list of directories
+
+    - dir.html
+        - template for building up the list of directory contents
+        - navigation buttons, current location listing
+
+- static folder:
+
+    - assets folder:
+        - Icons used on the pages
+
+    - files folder:
+        - All files to be served as content on the page
+
+
+Setup:
+- install all required dependencies via
+$ pip install requirements.txt
+
+- This script is meant to be running on an uWSGI server, preferrably behind nginx.
+    Quickstart docs: https://uwsgi-docs.readthedocs.io/en/latest/WSGIquickstart.html
+    nginx docs: https://www.nginx.com/resources/wiki/start/topics/tutorials/install/
+
+
+- Paste into the nginx config file (/etc/nginx/sites-enabled/YOUR_SITE.conf):
+
+    location / {
+        include uwsgi_params;
+        uwsgi_pass 127.0.0.1:3031;
+    }
+
+- Now add uwsgi to the services started at boot (Ubuntu uses Systemd, so the docs are at https://uwsgi-docs.readthedocs.io/en/latest/Systemd.html)
+
+- Use certbot to get a free Let'sEncrypt SSL certificate
+    Docs: https://certbot.eff.org/instructions?ws=nginx&os=ubuntufocal
+    - Certbot automatically refreshes the certificate when it expires.
+
+
+Testing the website locally:
+- install all required dependencies via
+$ pip install requirements.txt
+
+- Testing after this step is easy, just run this python script and open up http://127.0.0.1:5000 in your browser.
+
+'''
 
 @app.route("/")
 def home() :
     path = f"{os.getcwd()}/static/files/".replace("\\", "/")
-    #tempcontents = os.listdir(path)
-    #dircontents = []
-    #for x in tempcontents:
-    #    if "Klasse" in x:
-    #        sus = x.split("Klasse")
-    #        dircontents.append(f"{sus[1]}. Jahrgangsstufe")
-    #    elif x == "EA":
-    #        dircontents.append("Engine Alpha")
-    #    else:
-    #        dircontents.append(x)
-    #dircontents.sort()
     dircontents = ["Klasse6","Klasse7","Klasse9","Klasse10","Klasse11","Klasse12","EA"]
     fulllist = []
     for x in dircontents:
@@ -31,24 +91,12 @@ def home() :
         elif x == "EA":
                 temp.append("Engine Alpha")
         fulllist.append(temp)
-    #dircontents = ["6. Jahrgangsstufe","7. Jahrgangsstufe","9. Jahrgangsstufe","10. Jahrgangsstufe","11. Jahrgangsstufe","12. Jahrgangsstufe","Engine Alpha",]
     return render_template("index.html", dircontents= fulllist)
 
 
 @app.route("/<link>/<file>")
 def folder(link, file) :
-    #contents= []
-    #path = f"{os.getcwd()}/static/bruh/"
-    #for x in [os.path.join(root, name).split(path)
-    #         for root, dirs, files in os.walk(path)
-    #         for name in files]:
-    #             e= x[1].replace("\\","/")
-    #             contents.append(e)
-    #
-    #dir_list = next(os.walk(path))[1]
-    #
-    #
-    #return ''.join([f"{str(x)}-- " for x in dir_list])
+
     href = f"{link}*{file}"
     path = f"{os.getcwd()}/static/files/".replace("\\", "/")
     npath = link.replace("*", "/")
@@ -60,13 +108,6 @@ def folder(link, file) :
         npath = npath.replace("EngineAlpha", "EA", 1)
     dircont = []
     fullpath = f"{path}{npath}/{file}"
-    #for root, dirs, files in os.walk(f"{path}{npath}/{file}", topdown=True):
-    #    for name in files:
-    #        #if file not in root:
-    #        dircont.append(name.replace("\\","/"))
-    #    for name in dirs:
-    #        #if file in root:
-    #        dircont.append(name.replace("\\","/"))
 
     if "/" not in pth:
         pth = ""
