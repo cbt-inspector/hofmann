8bedd9196184fe182c2b0c8e1ba23dbfd4e0c280
moehreag <moehreag@gmail.com>
2022-02-12 21:40:33 +0100

    base.js

---
+++ static/js/base.js
@@ -0,0 +1,85 @@
+
+
+var dark
+let cookies = false
+
+function darkmode() {
+
+    var element = document.body;
+    element.classList.toggle("dark");
+
+
+
+    if (dark != true){
+
+    if (cookies){
+        Cookies.set('darkmode','true')
+    }
+    document.getElementById("darkbutton").innerHTML = "<img src=\"/static/assets/light.png\" width=\"30\" height=\"30\">"
+
+    document.getElementById("navicon").backgroundColor = "white"
+    dark = true
+
+    } else {
+    if (cookies){
+        Cookies.remove('darkmode')
+    }
+    dark = false
+    document.getElementById("darkbutton").innerHTML = "<img src=\"/static/assets/dark.png\" width=\"30\" height=\"30\">"
+
+    }
+
+    //window.location.reload()
+
+}
+
+function recoverDark() {
+
+    //let cookie = document.cookie
+
+    if(Cookies.get("cookies")=="true"){
+        cookies=true
+    }
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && darkmode === false){
+        darkmode()
+    }
+
+
+    if(Cookies.get('darkmode') == "true"){
+        var element = document.body;
+        element.classList.toggle("dark");
+
+        document.getElementById("darkbutton").innerHTML = "<img src=\"/static/assets/light.png\" width=\"30\" height=\"30\">"
+
+        dark = true
+    }
+    else {
+        dark = false
+
+    }
+
+}
+
+function togglePopup(){
+
+    var popup = document.getElementById("Popup");
+    popup.classList.toggle("show");
+
+}
+
+function acceptCookies(){
+    togglePopup()
+
+    Cookies.set('cookies', 'true')
+
+    cookies=true
+}
+
+function onLoad(){
+    recoverDark()
+
+    if(!cookies){
+        togglePopup()
+    }
+}
